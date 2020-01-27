15d86373af1937f585c6c7a83fb12f357f02600d
Ben Sutter <benjaminsutter@outlook.com>
2020-01-27 17:03:43 -0500

    Initial Commit. AST Builder and minimal grammar.

---
+++ src/main/kotlin/net/bms/orwell/tree/AST.kt
@@ -0,0 +1,54 @@
+package net.bms.orwell.tree
+
+import kotlin.collections.ArrayList
+
+class AST: Collection<AST> {
+    var parent: AST = this
+    var value: ASTValue = SpecialValues.NONE
+    val children = ArrayList<AST>()
+    fun addChild(child: AST): AST {
+        children.add(child)
+        child.parent = this
+        return children[children.size - 1]
+    }
+    fun addChildren(childList: Collection<AST>) {
+        childList.forEach {
+            children.add(it)
+            it.parent = this
+        }
+    }
+    fun getChild(id: Int) = children[id]
+
+    override fun contains(element: AST): Boolean {
+        var found = false
+        children.forEach {
+            if (it == element)
+                found = true
+            else if (!it.isEmpty())
+                found = it.contains(element)
+        }
+        return found
+    }
+
+    override fun containsAll(elements: Collection<AST>): Boolean {
+        if (isEmpty())
+            return false
+        elements.forEach {
+            if(!contains(it))
+                return false
+        }
+        return true
+    }
+
+    override fun isEmpty() = children.isEmpty()
+
+    override fun iterator():Iterator<AST> = iterator()
+    override val size: Int
+        get()  {
+            var num = 0
+            forEach {
+                num += it.size
+            }
+            return num
+        }
+}
